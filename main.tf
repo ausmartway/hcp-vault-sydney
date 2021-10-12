4e268a4a8792f243be43064de17b21b81e05d46b
Yulei Liu <yulei.liu@gmail.com>
2021-10-13 09:58:07 +1100

    test drive

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
 ##main.tf
+provider "hcp" {
+}
+
+// Create an HVN
+resource "hcp_hvn" "yulei-hvn" {
+  hvn_id         = "yulei-hvn"
+  cloud_provider = "aws"
+  region         = "ap-southeast-2"
+  cidr_block     = "172.25.16.0/20"
+}
+
+// Create a Vault cluster in the same region and cloud provider as the HVN
+resource "hcp_vault_cluster" "first-cluster" {
+  cluster_id = "first-cluster"
+  hvn_id     = hcp_hvn.example_hvn.hvn_id
+}
+
+resource "hcp_vault_cluster_admin_token" "admin" {
+  cluster_id = "first-cluster"
+}
