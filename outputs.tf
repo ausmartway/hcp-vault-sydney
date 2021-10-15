5f2e689f1efe83bf98f40f126fd60bda50a51bb7
Yulei Liu <yulei.liu@gmail.com>
2021-10-15 12:29:41 +1100

    enable token as output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@
 output "hcp-vault-public_endpoint" {
     value = hcp_vault_cluster.first-cluster.vault_public_endpoint_url
 }
+
+output "hcp-vault-admin-token" {
+    value = hcp_vault_cluster_admin_token.token
+}
