304b880701b4c2f5d2998e7209f4b830344b7ae5
Yulei Liu <yulei.liu@gmail.com>
2022-03-02 16:17:02 +1100

    test

--- main.tf
+++ main.tf
@@ -3,23 +3,23 @@ provider "hcp" {
 }
 
 // Create an HVN
-resource "hcp_hvn" "sydney" {
-  hvn_id         = "Sydney"
+resource "hcp_hvn" "vault" {
+  hvn_id         = "vault"
   cloud_provider = "aws"
   region         = "ap-southeast-2"
   cidr_block     = "172.25.16.0/20"
 }
 
 // Create a Vault cluster in the same region and cloud provider as the HVN
-resource "hcp_vault_cluster" "sydney-cluster" {
-  cluster_id = "sydney-cluster"
+resource "hcp_vault_cluster" "vault-cluster" {
+  cluster_id = "vault-cluster"
   public_endpoint = true
-  hvn_id     = hcp_hvn.sydney.hvn_id
+  hvn_id     = hcp_hvn.vault.hvn_id
 }
 
 resource "hcp_vault_cluster_admin_token" "admin" {
-  cluster_id = "sydney-cluster"
+  cluster_id = "vault-cluster"
   depends_on = [
-      hcp_vault_cluster.sydney-cluster,
+      hcp_vault_cluster.vault-cluster,
   ]
 }
