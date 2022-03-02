f3ede4c188e11a73435f2d5cdadb1bb7a1337703
Yulei Liu <yulei.liu@gmail.com>
2022-03-02 16:21:32 +1100

    rename hvn

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "hcp" {
 }
 
 // Create an HVN
-resource "hcp_hvn" "vault" {
-  hvn_id         = "vault"
+resource "hcp_hvn" "vault-demo" {
+  hvn_id         = "vault-demo"
   cloud_provider = "aws"
   region         = "ap-southeast-2"
   cidr_block     = "172.25.16.0/20"
@@ -14,7 +14,7 @@ resource "hcp_hvn" "vault" {
 resource "hcp_vault_cluster" "vault-cluster" {
   cluster_id = "vault-cluster"
   public_endpoint = true
-  hvn_id     = hcp_hvn.vault.hvn_id
+  hvn_id     = hcp_hvn.vault-demo.hvn_id
 }
 
 resource "hcp_vault_cluster_admin_token" "admin" {
