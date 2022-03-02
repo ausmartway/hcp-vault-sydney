bbfa72ba2e52fa67a224232dc65a62eca73e6772
Yulei Liu <yulei.liu@gmail.com>
2022-03-02 19:49:46 +1100

    subnet change

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 ##Add your terraform core and provider version constrains here.
 terraform {
   required_version = ">= 1.0"
-   required_providers {
-     hcp = ">= 0.17"
-   }
-  
+  required_providers {
+    hcp = ">= 0.17"
+  }
+
 }
