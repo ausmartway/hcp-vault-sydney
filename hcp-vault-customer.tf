bc7239e6b3e0ef2de3f46c11ee2999dec27021af
Yulei Liu <yulei.liu@gmail.com>
2025-02-15 14:10:55 -0600

    add plus tier cluster for more capabilities

---
+++ hcp-vault-customer.tf
@@ -0,0 +1,27 @@
+// Create a Vault cluster in the same region and cloud provider as the HVN
+resource "hcp_vault_cluster" "vault-plus-demo" {
+  cluster_id      = "vault-cluster"
+  tier = "Plus_small"
+  public_endpoint = true
+  hvn_id          = hcp_hvn.vault-demo-hvn.hvn_id
+}
+
+resource "hcp_vault_cluster_admin_token" "admin" {
+  cluster_id = "vault-cluster-demo"
+  depends_on = [
+    hcp_vault_cluster.vault-cluster-demo,
+  ]
+}
+
+output "vault-plus-demo-url" {
+  value = hcp_vault_cluster.vault-plus-demo.public_endpoint
+  description = "URL of Vault cluster"
+  sensitive = false
+}
+
+output "vault-plus-demo-admin-token" {
+  value = hcp_vault_cluster_admin_token.admin.admin_token
+  description = "Admin token for Vault cluster"
+  sensitive = false
+}
+
